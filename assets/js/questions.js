86de3379db93e0de3f6d7ff114c0395a141eb85e
shininglite <vandeusent@gmail.com>
2020-05-09 14:54:02 -0500

    add new questions to array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -6,8 +6,10 @@ function Question(title, choices, answer){
 }
 
 const q1 = new Question("Commonly used data types DO NOT include:", ["strings", "booleans", "alerts", "numbers"], 'alerts');
-const q2 = new Question("The condition in an if / else statement is enclosed within ____.", ["quotes", "curly brackets", "parentheses", "square brackets"], "paraentheses");
+const q2 = new Question("The condition in an if / else statement is enclosed within ____.", ["quotes", "curly brackets", "parentheses", "square brackets"], "parentheses");
 const q3 = new Question("Arrays in JavaScript can be used to store ____.", ["numbers and strings", "other arrays", "booleans", "all of the above"])
+const q4 = new Question("String values must be enclosed within ____ when being assigned to variables.", ["commas", "quotes", "curly brackets", "parentheses"], "quotes");
+const q5 = new Question("A very useful tool used during development and debugging for printing content to the debugger is ____", ["JavaScript", "terminal / bash", "for loops", "console.log"], "console.log");
 
 
 let questions = [q1, q2, q3];
